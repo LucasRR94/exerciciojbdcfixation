ff6c0aedc93b7e2776112858300ebcc3a3c7f32e
Lucas Rocha <lucasregodarocha@outlook.com>
2023-01-01 10:07:47 -0300

    feat: create the model entity Rent

---
+++ src/model/entities/Rent.java
@@ -0,0 +1,96 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Rent implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private Date currentMonth;
+	private int mallId;
+	private int departmentId;
+	private Double currentRent;
+	private Double currentPayedRent;
+	private boolean payed;
+	
+	public Rent(int id, Date currentMonth, int mallId, int departmentId, Double currentRent, Double currentPayedRent,
+			boolean payed) {
+		super();
+		this.id = id;
+		this.currentMonth = currentMonth;
+		this.mallId = mallId;
+		this.departmentId = departmentId;
+		this.currentRent = currentRent;
+		this.currentPayedRent = currentPayedRent;
+		this.payed = payed;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getCurrentMonth() {
+		return currentMonth;
+	}
+	public void setCurrentMonth(Date currentMonth) {
+		this.currentMonth = currentMonth;
+	}
+	public int getMallId() {
+		return mallId;
+	}
+	public void setMallId(int mallId) {
+		this.mallId = mallId;
+	}
+	public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+	public Double getCurrentRent() {
+		return currentRent;
+	}
+	public void setCurrentRent(Double currentRent) {
+		this.currentRent = currentRent;
+	}
+	public Double getCurrentPayedRent() {
+		return currentPayedRent;
+	}
+	public void setCurrentPayedRent(Double currentPayedRent) {
+		this.currentPayedRent = currentPayedRent;
+	}
+	public boolean isPayed() {
+		return payed;
+	}
+	public void setPayed(boolean payed) {
+		this.payed = payed;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rent other = (Rent) obj;
+		return id == other.id;
+	}
+	@Override
+	public String toString() {
+		return "Rent [id=" + id + ", currentMonth=" + currentMonth + ", mallId=" + mallId + ", departmentId="
+				+ departmentId + ", currentRent=" + currentRent + ", currentPayedRent=" + currentPayedRent + ", payed="
+				+ payed + "]";
+	}
+	
+}
