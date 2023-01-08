48e298f531afa924edf0527f9b39d49c545f09c9
Lucas Rocha <lucasregodarocha@outlook.com>
2023-01-08 11:49:34 -0300

    feat: create method for find all on Dao Rent

--- src/model/dao/impl/RentDaoJDBC.java
+++ src/model/dao/impl/RentDaoJDBC.java
@@ -134,7 +134,36 @@ public class RentDaoJDBC implements RentDao {
 	@Override
 	public List<Rent> findAllRent() {
 		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		List<Rent> allRents = new ArrayList<>();
+		try {
+			st = conn.prepareStatement(
+					"SELECT rent.*,rent.Id as Id, mall.Id as mallId, department_store.Id as departmentStoreId "
+							+ "FROM rent INNER JOIN mall "
+							+ "ON rent.MallId = mall.Id "
+							+ "INNER JOIN department_store "
+							+ "ON rent.DepartmentStoreId = department_store.Id "
+					);
+			rs = st.executeQuery();
+			Map <Integer, DepartmentStore> mapDep = new HashMap<>();
+			Map <Integer, Mall> mapMall = new HashMap<>();
+			while(rs.next()) {
+				DepartmentStore dep = mapDep.get(rs.getInt("departmentStoreId"));
+				Mall mall = mapMall.get(rs.getInt("mallId"));
+				if(dep == null) dep = new DepartmentStore(rs.getInt("departmentStoreId"));
+				if(mall == null) mall = new Mall(rs.getInt("mallId"));
+				allRents.add(new Rent(rs.getInt("rent.Id"),rs.getDate("rent.CurrentMonth"), mall, dep,rs.getDouble("rent.CurrentRent"),rs.getDouble("rent.currentPayedRent"),rs.getBoolean("rent.Payed")));
+			}
+			return allRents;
+
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
