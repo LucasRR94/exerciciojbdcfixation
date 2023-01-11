5b48b2f2ffcb0195eb139291a1416517bd6d5abf
Lucas Rocha <lucasregodarocha@outlook.com>
2023-01-10 21:37:39 -0300

    feat: create instantiation of createMallDao

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.MallDaoJDBC;
 import model.dao.impl.RentDaoJDBC;
 
 public class DaoFactory {
 	public static MallDao createMallDao() {
-		return null;
+		return new MallDaoJDBC(DB.getConnection());
 	}
 	public static RentDao createRentDao() {
 		return new RentDaoJDBC(DB.getConnection()); 
