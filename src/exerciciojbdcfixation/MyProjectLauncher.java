5b6e0e2b7e6c35864c49fd860883e58f8505397e
Lucas Rocha <lucasregodarocha@outlook.com>
2023-01-23 22:24:42 -0300

    feat: add feature for read the entire table in menu

--- src/exerciciojbdcfixation/MyProjectLauncher.java
+++ src/exerciciojbdcfixation/MyProjectLauncher.java
@@ -22,24 +22,25 @@ public class MyProjectLauncher {
 		int month = sc.nextInt();
 		System.out.println("Please type it the day in two digit format");
 		int day = sc.nextInt();
-		Map <Integer,Integer> mapMonths = Map.ofEntries(Map.entry(1,Calendar.JANUARY), 
-				Map.entry(2,Calendar.FEBRUARY),
-				Map.entry(3,Calendar.MARCH),
-				Map.entry(4,Calendar.APRIL),
-				Map.entry(5,Calendar.MAY),
-				Map.entry(6,Calendar.JUNE),
-				Map.entry(7,Calendar.JULY),
-				Map.entry(8,Calendar.AUGUST),
-				Map.entry(9,Calendar.SEPTEMBER),
-				Map.entry(10,Calendar.OCTOBER),
-				Map.entry(11,Calendar.NOVEMBER),
-				Map.entry(12,Calendar.DECEMBER));
+		Map<Integer, Integer> mapMonths = Map.ofEntries(Map.entry(1, Calendar.JANUARY),
+				Map.entry(2, Calendar.FEBRUARY),
+				Map.entry(3, Calendar.MARCH),
+				Map.entry(4, Calendar.APRIL),
+				Map.entry(5, Calendar.MAY),
+				Map.entry(6, Calendar.JUNE),
+				Map.entry(7, Calendar.JULY),
+				Map.entry(8, Calendar.AUGUST),
+				Map.entry(9, Calendar.SEPTEMBER),
+				Map.entry(10, Calendar.OCTOBER),
+				Map.entry(11, Calendar.NOVEMBER),
+				Map.entry(12, Calendar.DECEMBER));
 		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.YEAR,year);
+		cal.set(Calendar.YEAR, year);
 		cal.set(Calendar.MONTH, mapMonths.get(month));
 		cal.set(Calendar.DAY_OF_MONTH, day);
 		return cal.getTime();
 	}
+
 	static Rent createRent(Scanner sc) {
 		System.out.println("Please type it the current mounth of the Rent");
 		sc.nextLine();
@@ -50,8 +51,10 @@ public class MyProjectLauncher {
 		int idDepartmentStore = sc.nextInt();
 		System.out.println("Please type it the current Rent");
 		Double currentRent = sc.nextDouble();
-		return new Rent(0, currentMonth, new Mall(idMall), new DepartmentStore(idDepartmentStore),currentRent,0.00,false);
+		return new Rent(0, currentMonth, new Mall(idMall), new DepartmentStore(idDepartmentStore), currentRent, 0.00,
+				false);
 	}
+
 	static Mall createMall(Scanner sc) {
 		System.out.println("Please type it the name of the Mall");
 		sc.nextLine();
@@ -62,8 +65,9 @@ public class MyProjectLauncher {
 		String stateOfCountry = sc.nextLine();
 		System.out.println("Please type it the country of the Mall");
 		String country = sc.nextLine();
-		return new Mall(0,name,cityName,stateOfCountry,country);
+		return new Mall(0, name, cityName, stateOfCountry, country);
 	}
+
 	static DepartmentStore createDepartmentStore(Scanner sc) {
 		System.out.println("Please type it cnpj of department store");
 		sc.nextLine();
@@ -80,48 +84,80 @@ public class MyProjectLauncher {
 		int currentArea = sc.nextInt();
 		System.out.println("Please type it the mall id");
 		int mallId = sc.nextInt();
-		return new DepartmentStore(0,cnpj,name,email,creationDate,startedDate,currentArea,new Mall(mallId));
+		return new DepartmentStore(0, cnpj, name, email, creationDate, startedDate, currentArea, new Mall(mallId));
 	}
-	
-	static void insertMall(MallDao mall,Scanner sc) {
+
+	static void insertMall(MallDao mall, Scanner sc) {
 		mall.insertMall(createMall(sc));
 	}
-	
-	static void insertRent(RentDao rent,Scanner sc) {
+
+	static void insertRent(RentDao rent, Scanner sc) {
 		rent.insertRent(createRent(sc));
 	}
-	
-	static void insertDepartmentStore(DepartmentStoreDao dep,Scanner sc) {
+
+	static void insertDepartmentStore(DepartmentStoreDao dep, Scanner sc) {
 		dep.insertDepartmentStore(createDepartmentStore(sc));
 	}
-	static void insertingOnTable(MallDao mall,RentDao rent,DepartmentStoreDao dep,Scanner sc) {
+
+	static void insertingOnTable(MallDao mall, RentDao rent, DepartmentStoreDao dep, Scanner sc) {
 		System.out.println("Type 1 for Inserting on Department Store");
 		System.out.println("Type 2 for Inserting on Rent");
 		System.out.println("Type 3 for Inserting on mall");
 		int option = sc.nextInt();
 		switch (option) {
-		case 1:
-			insertDepartmentStore(dep,sc);
-			break;
-		case 2:
-			insertRent(rent,sc);
-			break;
-		case 3:
-			insertMall(mall,sc);
-			break;
-		default:
-			break;
+			case 1:
+				insertDepartmentStore(dep, sc);
+				break;
+			case 2:
+				insertRent(rent, sc);
+				break;
+			case 3:
+				insertMall(mall, sc);
+				break;
+			default:
+				break;
+		}
+
 	}
-		
+
+	static void readDepartmentStore(DepartmentStoreDao dep) {
+		System.out.println(dep.findAllDepartmentStore());
 	}
-	static void readTable() {
-		
+
+	static void readMall(MallDao mall) {
+		System.out.println(mall.findAllMall());
 	}
+
+	static void readRent(RentDao rent) {
+		System.out.println(rent.findAllRent());
+	}
+
+	static void readTable(MallDao mall, RentDao rent, DepartmentStoreDao dep, Scanner sc) {
+		System.out.println("Type 1 for reading on Department Store");
+		System.out.println("Type 2 for reading on Rent");
+		System.out.println("Type 3 for reading on mall");
+		int option = sc.nextInt();
+		switch (option) {
+			case 1:
+				readDepartmentStore(dep);
+				break;
+			case 2:
+				readRent(rent);
+				break;
+			case 3:
+				readMall(mall);
+				break;
+			default:
+				break;
+		}
+	}
+
 	static void deleteRecord() {
-		
+
 	}
+
 	static void updateRecord() {
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -130,7 +166,7 @@ public class MyProjectLauncher {
 		MallDao mallDao = DaoFactory.createMallDao();
 		DepartmentStoreDao departmentStoreDao = DaoFactory.createDepartmentStoreDao();
 		boolean cond = true;
-		while(cond){
+		while (cond) {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Type 1 for Inserting on table");
 			System.out.println("Type 2 for Read table");
@@ -139,24 +175,21 @@ public class MyProjectLauncher {
 			int option = sc.nextInt();
 			switch (option) {
 				case 1:
-					insertingOnTable(mallDao,rentDao,departmentStoreDao,sc);
+					insertingOnTable(mallDao, rentDao, departmentStoreDao, sc);
 					break;
 				case 2:
-					readTable();
+					readTable(mallDao, rentDao, departmentStoreDao, sc);
 					break;
 				case 3:
 					deleteRecord();
 					break;
 				default:
-					cond = false; 
+					cond = false;
 					break;
 			}
-			
-		}
-		
 
-		
-		
+		}
+
 	}
 
 }
